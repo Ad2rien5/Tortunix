51d8381ad46b97a6cc6b8708030a5c87163628a8
Adrien SEACH <adrien.seach2024@campus-eni.fr>
2025-10-08 09:15:12 +0200

    ¯\_(°_°)_/¯

--- src/lib/obj/instruction.ml
+++ src/lib/obj/instruction.ml
@@ -2,9 +2,11 @@
 exception Instruction_Exception of string
 
 
-
 class instruction : 
     object 
         val name : string
+        val param1int : int
+        val param2int : int
+        val
         method verify : int -> int
     end
