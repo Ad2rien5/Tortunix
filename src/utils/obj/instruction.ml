c6c1fd111c6ec8f07b27445d2cfac699847485f1
Ad2rien5 <adrien.seach2024@campus-eni.fr>
2026-01-09 09:37:53 +0100

    Reshape architecture

--- src/utils/obj/instruction.ml
+++
@@ -1,6 +0,0 @@
-class instruction (name: string) =
-    object (self)
-        val mutable param1int : int
-        val mutable param2int : int
-        method verify : int -> int
-    end;;
--- src/utils/obj/parameter.ml
+++
@@ -1,6 +0,0 @@
-class parameter (paramtype : string) =
-    object (self)
-        val mutable valint = 0
-        val mutable valstr = ""
-        val mutable valpos = 0
-    end;;
